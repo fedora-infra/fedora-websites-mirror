cb6880e788f62a3f68a7a9725d5484bd687cf20c
Rick Elrod <rick@elrod.me>
2020-02-26 21:57:22 -0600

    Attempt to fix production by cloning translations with https instead of ssh
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- sites/getfedora.org/scripts/pull-translations.sh
+++ sites/getfedora.org/scripts/pull-translations.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 TRANS_TMP=$(mktemp -d)
 
-git clone ssh://git@pagure.io/fedora-web/translations.git "$TRANS_TMP"
+git clone https://pagure.io/fedora-web/translations.git "$TRANS_TMP"
 mkdir -p translations/
 cp -R "$TRANS_TMP"/getfedora.org-redesign/* translations/
 rm -rf "$TRANS_TMP"
