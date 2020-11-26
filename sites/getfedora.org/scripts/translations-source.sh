7003c5cb7ddc51aab87e76b3ef4906951b78fb80
Jan Kuparinen <copper_fin@hotmail.com>
2020-11-26 18:05:49 +0200

    updated path references so script will now work and creates a new getfedore.org.pot

--- sites/getfedora.org/scripts/translations-source.sh
+++ sites/getfedora.org/scripts/translations-source.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-pybabel extract -F ../babel.cfg -o getfedora.org.pot . ../partials/
+#pybabel extract -F ../babel.cfg -o getfedora.org.pot . ../partials/
+pybabel extract -F ../../babel.cfg -o getfedora.org.pot ../../partials ../../getfedora.org/site ../../getfedora.org/partials
 echo
 echo
 echo 'If you have write access to https://pagure.io/fedora-web/translations, you may now run:'
