5f168c1e9411adb292dc405da57f3b198d932f2d
Francois Andrieu <darknao@fedoraproject.org>
2021-10-08 23:12:49 +0200

    disable automatic pot update for now
    POT update is running everyday on infra with an obsolete version of
    babel that break translation.
    A proper desactivation is planned here https://pagure.io/fedora-infra/ansible/pull-request/823
    but until it's merged, I'm disabling it here to prevent breakage during
    release period.

--- sites/getfedora.org/scripts/translations-source.sh
+++ sites/getfedora.org/scripts/translations-source.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#pybabel extract -F ../babel.cfg -o getfedora.org.pot . ../partials/
-pybabel extract -F ../../babel.cfg -o getfedora.org.pot ../../partials ../../getfedora.org/site ../../getfedora.org/partials
+# Prevent updating pot until https://pagure.io/fedora-infra/ansible/pull-request/823 is merged
+#pybabel extract -F ../../babel.cfg -o getfedora.org.pot ../../partials ../../getfedora.org/site ../../getfedora.org/partials
 echo
 echo
 echo 'If you have write access to https://pagure.io/fedora-web/translations, you may now run:'
