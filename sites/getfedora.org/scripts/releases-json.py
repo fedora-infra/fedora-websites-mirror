99e3fe603f615337272d9301f5bcb85e6f1bbeae
Francois Andrieu <darknao@fedoraproject.org>
2023-03-15 17:28:16 +0100

    add IoT to FMW

--- sites/getfedora.org/scripts/releases-json.py
+++ sites/getfedora.org/scripts/releases-json.py
@@ -7,9 +7,12 @@ import yaml
 
 r = {}
 output = []
+IOT_URL = "https://download.fedoraproject.org/pub/alt/iot"
+IOT_BETA_URL = "https://download.fedoraproject.org/pub/alt/iot/test"
 
 with open(os.path.join(os.path.dirname(__file__), '..', 'release.yaml')) as data:
     r = yaml.safe_load(data)
+
 
 def hashify(version, milestone, arch, link, variant, subvariant):
     return { 'version': version
@@ -18,6 +21,7 @@ def hashify(version, milestone, arch, link, variant, subvariant):
            , 'variant': variant
            , 'subvariant': subvariant
            }
+
 
 releases_to_report = []
 for release in r.get('fmw', {}).get('releases_json', []):
@@ -31,6 +35,18 @@ for release in r.get('fmw', {}).get('releases_json', []):
         fedfind.release.get_release(
             int(version),
             milestone=milestone))
+    
+    # IoT hack-ish special case
+    iot = None
+    if milestone == 'final':
+        iot = fedfind.release.get_release(url="/".join((IOT_URL, str(version))))
+        iot._version = str(version)
+    else:
+        iot = fedfind.release.get_release(url="/".join((IOT_BETA_URL, str(version))))
+        iot._version = f"{version} {milestone}"
+    iot.__class__ = fedfind.release.Pungi4Mirror
+    releases_to_report.append(iot)
+
 
 for rel in releases_to_report:
     for img in rel.all_images:
